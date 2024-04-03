512d175a2181c3f975e18bc08dd32985c8670ab1
avanish-111 <tavanish09@gmail.com>
2024-04-03 13:18:40 +0530

    first commit

---
+++ add(1).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c,d=2;
+    printf("Enter two numbers");
+    scanf("%d%d",&a,&b);
+    c=a+b+d;
+    printf("%d",c);
+    return 0;
+}
